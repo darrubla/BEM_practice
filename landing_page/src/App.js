cf68f266106bc5b96b98c0a26dacb9b500dd844e
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-06-05 09:12:26 -0500

    first commit 05/06/2020

---
+++ landing_page/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <img
+        id="myGif"
+        src="https://media.giphy.com/media/e7FOBuKCDtwWI/giphy.gif"
+        alt="darth_maul"
+      />
+      <div className="App">
+        <div className="App-container">
+          <div className="App-content">
+            <div className="App-title">
+              <h1>Hello There!</h1>
+              <p>Wellcome to the Dark Side</p>
+              <p>Visitor #{Math.floor(Math.random() * 10000)}</p>
+            </div>
+            <div className="App-image">
+              <figure>
+                <img
+                  src="https://cdn4.iconfinder.com/data/icons/helmet-1/154/darth-vader-mask-helmet-darkness-knight-512.png"
+                  alt="darth_vader"
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
